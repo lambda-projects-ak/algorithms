a4b38a8d1b5f429a2cdfdb9a4bac0733c67d08ce
Kingaa6 <Kingaa6@gmail.com>
2019-05-01 14:17:29 -0700

    Add recipe_batches solution

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    comparison = []
+    if (recipe.keys() == ingredients.keys()):
+        for key in sorted(ingredients.keys()):
+            comparison.append((ingredients[key]//recipe[key]))
+        print(comparison)
+        if min(comparison) <= 0:
+            return 0
+        else:
+            return min(comparison)
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
