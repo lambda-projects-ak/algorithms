67f0b011b92e1a7dfa7535868cecc3aec4dadba3
Kingaa6 <Kingaa6@gmail.com>
2019-05-01 17:03:54 -0700

    eating_cookies solution added

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -11,7 +11,24 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    pass
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n + 1)}
+        cache[n] = eating_cookies(
+            n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+        return cache[n]
+
+
+print(eating_cookies(50))
+
 
 
 if __name__ == "__main__":
